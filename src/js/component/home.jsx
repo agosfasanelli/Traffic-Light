a4def8568084f5105af0f70b67619daefd35312c
agosfasanelli <agosfasanelli@gmail.com>
2022-11-07 20:32:39 +0000

    hsjdgf

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,57 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const palo = {
+		backgroundColor: "black",
+        width: "15px",
+        height: "60px",
+		textAlgin: "center",
+		}
+	const trafficLights = {
+        backgroundColor: "black",
+        width: "110px",
+        height: "300px",
+		textAlgin: "center",
+		marginLeft: "45%",
+		borderRadius: "8%"
+    }
+	const redLight = {
+		backgroundColor: "red",
+		borderRadius: "50%",
+		width: "100px",
+        height: "100px",
+		marginLeft: "4%"
+	}
+	const yellowLight = {
+		backgroundColor: "yellow",
+		borderRadius: "50%",
+		width: "100px",
+        height: "100px",
+		textAlgin: "center",		
+		marginLeft: "4%"
+	}
+	const greenLight = {
+		backgroundColor: "green",
+		borderRadius: "50%",
+		width: "100px",
+        height: "100px",
+		textAlgin: "center",
+		marginLeft: "4%"
+	}
+
+	
+	return(
+        <div className="Semasforo">
+			<div style={palo}></div>
+			<div style = {trafficLights}>
+				<div style={redLight}></div>
+				<div style={yellowLight}></div>
+				<div style={greenLight}></div>
+			</div>
+			
+        </div>
+    )
 };
 
 export default Home;
