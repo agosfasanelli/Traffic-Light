e7cf3863d9e2640c94c80db9ef6082057fabb5ea
agosfasanelli <agosfasanelli@gmail.com>
2022-11-09 10:09:33 +0000

    hdsgdj

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,56 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Home = () => {
-	const palo = {
-		backgroundColor: "black",
-        width: "15px",
-        height: "60px",
-		textAlgin: "center",
-		}
-	const trafficLights = {
-        backgroundColor: "black",
-        width: "110px",
-        height: "300px",
-		textAlgin: "center",
-		marginLeft: "45%",
-		borderRadius: "8%"
-    }
-	const redLight = {
-		backgroundColor: "red",
-		borderRadius: "50%",
-		width: "100px",
-        height: "100px",
-		marginLeft: "4%"
-	}
-	const yellowLight = {
-		backgroundColor: "yellow",
-		borderRadius: "50%",
-		width: "100px",
-        height: "100px",
-		textAlgin: "center",		
-		marginLeft: "4%"
-	}
-	const greenLight = {
-		backgroundColor: "green",
-		borderRadius: "50%",
-		width: "100px",
-        height: "100px",
-		textAlgin: "center",
-		marginLeft: "4%"
-	}
-
 	
+	const [light, setLight] = useState()
+
+		const lightColor = (event) => {
+		setLight(event.target.id)
+		}
+
 	return(
-        <div className="Semasforo">
-			<div style={palo}></div>
-			<div style = {trafficLights}>
-				<div style={redLight}></div>
-				<div style={yellowLight}></div>
-				<div style={greenLight}></div>
+        <div className="Semaforo">
+			<div id="palo"></div>
+			<div id = "trafficLights">
+				<div id="redLight" onClick={lightColor}>
+				</div>
+				<div id="yellowLight" onClick={lightColor}>
+				</div>
+				<div id="greenLight" onClick={lightColor}>
+				</div>
 			</div>
-			
-        </div>
+		</div>
     )
 };
 
