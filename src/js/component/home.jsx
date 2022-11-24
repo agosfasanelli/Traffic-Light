9dfb6329003847a09fd4aebdf8fe8602c3fdc247
agosfasanelli <agosfasanelli@gmail.com>
2022-11-24 10:42:27 +0000

    vcsgdhva

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,32 +1,49 @@
-import React, { useState } from "react";
-import Light from "./light";
+import React, {useState} from "react";
 
+//include images into your bundle
 
+//create your first component
 const Home = () => {
 
-	const [stateLight, setStateLight] = useState ({
-        red: "off",
-        yellow: "off",
-        green: "off"
-    })
-
-   
-	return(
-		<div className="Semaforo">
-			<div id="palo"></div>
-			<div id="trafficLights">
-				<Light 
-				 
-				/>
-				<Light 
-				
-				/>
-				<Light 
-				
-				/>
+	const [red, sertRed] = useState("red")
+	const handleClick1 = () => {
+		sertRed("redOn")
+		setYellow("yellow")
+		setGreen("green")
+	}
+	
+	const [yellow, setYellow] = useState("yellow")
+	const handleClick2 = () => {
+		setYellow("yellowOn")
+		sertRed("red")
+		setGreen("green")
+	}
+	const [green, setGreen] = useState("green")
+	const handleClick3 = () => {
+		setGreen("greenOn")
+		sertRed("red")
+		setYellow("yellow")
+	}
+	return (
+		<div className="container">
+			<div className="palo"></div>
+			<div className="trafficLights">
+			<div class="text-center">
+				<button onClick={handleClick1}className={red}></button>
 			</div>
-    	</div>
-		)
+			<div class="text-center">
+				<button onClick={handleClick2}className={yellow}></button>
+			</div>
+			<div class="text-center">
+				<button onClick={handleClick3}className={green}></button>
+			</div>
+			</div>
+			</div>
+		
+			
+		
+	
+	);
 };
 
 export default Home;
--- src/js/component/light.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Light = (props) => {
-
-
-    return ()
-}
-
-export default Light
